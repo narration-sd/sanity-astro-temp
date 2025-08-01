e1e3f294428926c0e804c829c1513757c0fa3c88
Narration SD <narration-sd@users.noreply.github.com>
2025-08-01 10:09:20 -0700

    many things commented out, unused packages removed

--- apps/blog/sanity.config.ts
+++ apps/blog/sanity.config.ts
@@ -1,83 +1,83 @@
 import { defineConfig } from "sanity";
 
-import { structureTool} from "sanity/structure";
-import { visionTool } from "@sanity/vision";
-import {
-  defineDocuments,
-  defineLocations,
-  presentationTool,
-  type DocumentLocation,
-} from "sanity/presentation";
-import { media } from 'sanity-plugin-media'
-import { unsplashImageAsset } from 'sanity-plugin-asset-source-unsplash'
+// import { structureTool} from "sanity/structure";
+// import { visionTool } from "@sanity/vision";
+// import {
+//   defineDocuments,
+//   defineLocations,
+//   presentationTool,
+//   type DocumentLocation,
+// } from "sanity/presentation";
+// import { media } from 'sanity-plugin-media'
+// import { unsplashImageAsset } from 'sanity-plugin-asset-source-unsplash'
+//
+// import { schemaTypes } from "./schemas";
 
-import { schemaTypes } from "./schemas";
-
-export const projectId = typeof process !== 'undefined'
-  ? process.env.SANITY_STUDIO_PROJECT_ID
-  : import.meta.env.PUBLIC_SANITY_PROJECT_ID
-
-export const dataset =  typeof process !== 'undefined'
-  ? process.env.SANITY_STUDIO_DATASET
-  : import.meta.env.PUBLIC_SANITY_DATASET
-
-const SANITY_STUDIO_PREVIEW_URL = typeof process !== 'undefined'
-  ? process.env.SANITY_STUDIO_PREVIEW_URL
-  : import.meta.env.PUBLIC_SANITY_STUDIO_PREVIEW_URL
-
-const homeLocation = {
-  title: "Home",
-  href: "/",
-} satisfies DocumentLocation;
+// export const projectId = typeof process !== 'undefined'
+//   ? process.env.SANITY_STUDIO_PROJECT_ID
+//   : import.meta.env.PUBLIC_SANITY_PROJECT_ID
+//
+// export const dataset =  typeof process !== 'undefined'
+//   ? process.env.SANITY_STUDIO_DATASET
+//   : import.meta.env.PUBLIC_SANITY_DATASET
+//
+// const SANITY_STUDIO_PREVIEW_URL = typeof process !== 'undefined'
+//   ? process.env.SANITY_STUDIO_PREVIEW_URL
+//   : import.meta.env.PUBLIC_SANITY_STUDIO_PREVIEW_URL
+//
+// const homeLocation = {
+//   title: "Home",
+//   href: "/",
+// } satisfies DocumentLocation;
 
 export default defineConfig({
   name: "sanity-astro",
   title: "Sanity Astro",
-  projectId: projectId,
-  dataset: dataset,
+  // projectId: projectId,
+  // dataset: dataset,
   plugins: [
-    structureTool(),
-    visionTool(),
-    presentationTool({
-      previewUrl: SANITY_STUDIO_PREVIEW_URL,
-      title: 'Presentation',
-      resolve: {
-        mainDocuments: defineDocuments([
-          {
-            route: "/posts/:slug",
-            filter: `_type == "post" && (slug.current == $slug || _id == $slug)`,
-          },
-        ]),
-        locations: {
-          settings: defineLocations({
-            locations: [homeLocation],
-            message: "This document is used on all pages",
-            tone: "caution",
-          }),
-          post: defineLocations({
-            select: {
-              title: "title",
-              slug: "slug.current",
-            },
-            resolve: (doc) => ({
-              locations: [
-                doc
-                  ? {
-                    title: doc?.title || "Untitled",
-                    href: `/posts/${doc.slug}`,
-                  }
-                  : null,
-                homeLocation,
-              ].filter(Boolean) as DocumentLocation[],
-            }),
-          }),
-        },
-      },
-    }),
-    media(),
-    unsplashImageAsset(),
+    // structureTool(),
+    // visionTool(),
+    // presentationTool({
+    //   previewUrl: SANITY_STUDIO_PREVIEW_URL,
+    //   title: 'Presentation',
+    //   resolve: {
+    //     mainDocuments: defineDocuments([
+    //       {
+    //         route: "/posts/:slug",
+    //         filter: `_type == "post" && (slug.current == $slug || _id == $slug)`,
+    //       },
+    //     ]),
+    //     locations: {
+    //       settings: defineLocations({
+    //         locations: [homeLocation],
+    //         message: "This document is used on all pages",
+    //         tone: "caution",
+    //       }),
+    //       post: defineLocations({
+    //         select: {
+    //           title: "title",
+    //           slug: "slug.current",
+    //         },
+    //         resolve: (doc) => ({
+    //           locations: [
+    //             doc
+    //               ? {
+    //                 title: doc?.title || "Untitled",
+    //                 href: `/posts/${doc.slug}`,
+    //               }
+    //               : null,
+    //             homeLocation,
+    //           ].filter(Boolean) as DocumentLocation[],
+    //         }),
+    //       }),
+    //     },
+    //   },
+    // }),
+    // media(),
+    // unsplashImageAsset(),
   ],
-  schema: {
-    types: schemaTypes,
-  },
+  // schema: {
+  //   types: schemaTypes,
+  // },
 });
