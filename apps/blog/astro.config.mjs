353b7cfc05036c7dc906306df3ff69a2358b9f46
Narration SD <narration-sd@users.noreply.github.com>
2025-08-01 10:32:40 -0700

    very minimal now, still showing the EPERM error when Vite loads studio and tries to rename deps variants in its node_modules/.vite folder.

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -1,6 +1,5 @@
 import sanityIntegration from "@sanity/astro";
 import { defineConfig } from "astro/config";
-// import node from '@astrojs/node';
 import netlify from '@astrojs/netlify';
 import react from "@astrojs/react";
 // import { loadEnv } from 'vite'
@@ -12,28 +11,17 @@ const env = {
   // ])
 }
 
-// 'sanity' check...
-// if (!(env.PUBLIC_SANITY_PROJECT_ID && env.PUBLIC_SANITY_DATASET
-//   && env.PUBLIC_SANITY_API_READ_TOKEN && env.PUBLIC_SANITY_API_VERSION)) {
-//   throw new Error ('You have to fill in all your /.env.development environmental variables, ' +
-//       'from the example in /.env_example, before you can run this site...'
-//   )
-// }
-
-// https://astro.build/config
-
 const studioPath = '/studio'
 export default defineConfig({
   integrations: [
     sanityIntegration({
       projectId: '1111aaaa', // env.PUBLIC_SANITY_PROJECT_ID,
       dataset: 'production', // env.PUBLIC_SANITY_DATASET,
-      apiVersion: 'v2025-06-10', // env.PUBLIC_SANITY_API_VERSION || ,
+      apiVersion: 'v2025-08-01', // env.PUBLIC_SANITY_API_VERSION || ,
       useCdn: false,
-      // perspective: 'drafts',
-      // token: env.PUBLIC_SANITY_API_READ_TOKEN,
+
+      // these are necessary to enable the embedded studio, not otherwise used
       stega: {
-        // enabled:true,
         studioUrl: studioPath, // env.PUBLIC_SANITY_STUDIO_BASE_PATH,
       },
       studioBasePath: studioPath, // env.PUBLIC_SANITY_STUDIO_BASE_PATH,
@@ -42,12 +30,5 @@ export default defineConfig({
   ],
   vite: { resolve: { alias: { lodash : 'lodash-es' } } },
   output: "server",
-  // adapter: node({
-  //   mode: 'standalone'
-  // }),
   adapter: netlify(),
 });
-
-// n.b. pnpm run build normally, then run the result,
-// using node dist/server/entry.mjs
-// this will operate normally -- but show the bug!
